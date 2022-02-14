8093f0f391d42b83a6840ce6d1e1adcccad8a13e
Luis Antonio Parrado <luisprmat@gmail.com>
2022-02-14 00:53:57 -0500

    Get pokemon names from API with axios

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -11,8 +11,6 @@ import PokemonPicture from '@/components/PokemonPicture'
 
 import getPokemonOptions from '@/helpers/getPokemonOptions'
 
-console.log(getPokemonOptions())
-
 export default {
   components: { PokemonPicture, PokemonOptions }
 }
