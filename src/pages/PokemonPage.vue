dcd2668823dc09afa19c8ed2f20997c95ed11915
Luis Antonio Parrado <luisprmat@gmail.com>
2022-02-21 22:04:51 -0500

    Result and restart game

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -13,6 +13,14 @@
       :pokemons="pokemonArr"
       @selection="checkAnswer"
     />
+
+    <template v-if="showAnswer">
+      <h2 class="fade-in">{{ message }}</h2>
+
+      <button @click="newGame">
+        Nuevo juego
+      </button>
+    </template>
   </div>
 </template>
 
@@ -28,7 +36,9 @@ export default {
     return {
       pokemonArr: [],
       pokemon: null,
-      showPokemon: false
+      showPokemon: false,
+      showAnswer: false,
+      message: ''
     }
   },
   methods: {
@@ -38,8 +48,22 @@ export default {
       const rndInt = Math.floor(Math.random() * 4)
       this.pokemon = this.pokemonArr[ rndInt ]
     },
-    checkAnswer(pokemonId) {
+    checkAnswer( selectedId ) {
       this.showPokemon = true
+      this.showAnswer = true
+
+      if (selectedId === this.pokemon.id) {
+        this.message = `Correcto, ${ this.pokemon.name }`
+      } else {
+        this.message = `Oops, era ${ this.pokemon.name }`
+      }
+    },
+    newGame() {
+      this.showPokemon = false
+      this.showAnswer = false
+      this.pokemonArr = []
+      this.pokemon = null
+      this.mixPokemonArr()
     }
   },
   mounted() {
